646a5e2b5f3457c17b535a60c4a9f2d221da8339
fl-fabio <fl.fabio.lanzafame@gmail.com>
2024-02-16 15:59:44 +0100

    feature(): added pruduct interface

---
+++ src/models/post.model.ts
@@ -0,0 +1,10 @@
+export interface Product {
+    id: number;
+    title: string;
+    userId: string;
+    body: string;
+    img: string;
+    price: number;
+    category: string;
+    quantity: number;
+}
